29917d80611c6ad12943de198b63181f6e98f37d
Simon Rogers <simon.rogers@onthebeach.co.uk>
2017-03-23 21:50:46 +0000

    Added match events, controller and service

---
+++ src/FMS.Site/Services/IMatchEventsService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using FMS.Site.Data;
+using FMS.Site.Models;
+
+namespace FMS.Site.Services
+{
+    public interface IMatchEventsService
+    {
+        IEnumerable<MatchEvent> GetForMatch(int matchId);
+    }
+
+    public class MatchEventsService : IMatchEventsService
+    {
+        public IEnumerable<MatchEvent> GetForMatch(int matchId)
+        {
+            return MatchEventsData.GetForMatch(matchId);
+        }
+    }
+}
