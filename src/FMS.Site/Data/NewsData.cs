098984d43ceacf2d153218ae21472ba4852d7cc7
Simon Rogers <simon.rogers@onthebeach.co.uk>
2017-04-02 06:58:23 +0100

    Added model, service, controller for news items

---
+++ src/FMS.Site/Data/NewsData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FMS.Site.Models;
+
+namespace FMS.Site.Data
+{
+    public static class NewsData
+    {
+        public static List<NewsItem> NewsItems = new List<NewsItem>();
+
+        public static void AddNewsItem(int seasonId, int weekId, int divisionId, 
+                                    int teamId, int playerId, string news)
+        {
+            NewsItems.Add(new NewsItem
+            {
+                Id = GetNextId(),
+                SeasonId = seasonId,
+                WeekId = weekId,
+                News = news
+            });
+        }
+
+        private static int GetNextId()
+        {
+            return !NewsItems.Any() ? 1 : NewsItems.Max(n => n.Id) + 1;
+        }
+
+        public static IEnumerable<NewsItem> GetForCurrentWeek()
+        {
+            return NewsItems.Where(n => n.SeasonId == GameData.CurrentSeason &&
+                                        n.WeekId == GameData.CurrentWeek);
+        }
+
+        public static IEnumerable<NewsItem> GetMostRecent(int number)
+        {
+            return NewsItems.OrderByDescending(n => n.Id).Take(number);
+        }
+    }
+}
---
+++ src/FMS.Site/Models/newsItem.cs
@@ -0,0 +1,10 @@
+﻿namespace FMS.Site.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        public int SeasonId { get; set; }
+        public int WeekId { get; set; }
+        public string News { get; set; }
+    }
+}
